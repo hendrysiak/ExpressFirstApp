e955671af51be708df23905087532f323250a42a
hendrysiak <lukasz.hendrysiak@gmail.com>
2019-08-18 18:31:27 +0200

    Try remote repository

--- routes/quiz.js
+++ routes/quiz.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const Quiz = require('../models/quiz')
 
 /* GET home page. */
 router.get('/', (req, res) => {
+    new Quiz({
+        title: `Pytanie1`,
+        vote: 0
+    })
     res.render('quiz', {
         title: 'Quiz'
     });
